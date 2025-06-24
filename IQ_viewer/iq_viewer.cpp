fff1ba5952c64219c6e4669d7ba06656320a4bc4
AdamPalecek <44730891+AdamPalecek@users.noreply.github.com>
2025-06-24 11:26:07 +0200

    FFT - lib

--- IQ_viewer/iq_viewer.cpp
+++ IQ_viewer/iq_viewer.cpp
@@ -1,11 +1,19 @@
 #include "iq_viewer.h"
 #include "./ui_iq_viewer.h"
 
+// Add this block to include kissfft headers
+extern "C" {
+#include "kiss_fft.h"
+#include "kiss_fftr.h"
+}
+
 IQ_viewer::IQ_viewer(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::IQ_viewer)
 {
     ui->setupUi(this);
+
+    // You can now use kiss_fft functions here
 }
 
 IQ_viewer::~IQ_viewer()
