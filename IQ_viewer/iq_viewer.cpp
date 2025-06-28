9caffeff3d196148357e773798643a4f1e5260e1
AdamPalecek <44730891+AdamPalecek@users.noreply.github.com>
2025-06-28 10:56:28 +0200

    Ui changes- colors, plot interaction

--- IQ_viewer/iq_viewer.cpp
+++ IQ_viewer/iq_viewer.cpp
@@ -5,11 +5,12 @@
 #include <QDebug>
 #include <vector>
 #include <complex>
+#include <cmath>
 
-// Add this block to include kissfft headers
+//kissfft headers
 extern "C" {
 #include "kiss_fft.h"
-#include "kiss_fftr.h"
+//#include "kiss_fftr.h"
 }
 
 IQ_viewer::IQ_viewer(QWidget *parent)
@@ -20,6 +21,8 @@ IQ_viewer::IQ_viewer(QWidget *parent)
 {
     ui->setupUi(this);
 
+    setupUiControls();
+
     // --- QCustomPlot setup ---
     ui->fftPlot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom);
     ui->fftPlot->axisRect()->setupFullAxesBox(true);
@@ -29,20 +32,6 @@ IQ_viewer::IQ_viewer(QWidget *parent)
     m_colorMap = new QCPColorMap(ui->fftPlot->xAxis, ui->fftPlot->yAxis);
     m_colorMap->data()->setSize(1, m_fft_size);
     m_colorMap->data()->setRange(QCPRange(0, 1), QCPRange(0, m_fft_size));
-    m_colorMap->setGradient(QCPColorGradient::gpSpectrum);
-
-    // --- Slider Connections ---
-    connect(ui->timelineSlider, &QSlider::valueChanged, this, &IQ_viewer::on_timelineSlider_valueChanged);
-    connect(ui->minLevelSlider, &QSlider::valueChanged, this, &IQ_viewer::on_minLevelSlider_valueChanged);
-    connect(ui->maxLevelSlider, &QSlider::valueChanged, this, &IQ_viewer::on_maxLevelSlider_valueChanged);
-
-
-    // --- Slider Initial Configuration ---
-    ui->minLevelSlider->setRange(-120, 0);
-    ui->maxLevelSlider->setRange(-120, 0);
-    ui->minLevelSlider->setValue(-100);
-    ui->maxLevelSlider->setValue(-20);
-
 
     ui->fftPlot->rescaleAxes();
     ui->fftPlot->replot();
@@ -55,9 +44,64 @@ IQ_viewer::~IQ_viewer()
     delete ui;
 }
 
+void IQ_viewer::setupUiControls()
+{
+    // --- Connections ---
+    connect(ui->timelineSlider, &QSlider::valueChanged, this, &IQ_viewer::on_timelineSlider_valueChanged);
+    connect(ui->minLevelSlider, &QSlider::sliderReleased, this, &IQ_viewer::updateColorMapRange);
+    connect(ui->maxLevelSlider, &QSlider::sliderReleased, this, &IQ_viewer::updateColorMapRange);
+    connect(ui->colorMapComboBox, QOverload<int>::of(&QComboBox::currentIndexChanged), this, &IQ_viewer::on_colorMapComboBox_currentIndexChanged);
+    connect(ui->fftSizeComboBox, QOverload<int>::of(&QComboBox::currentIndexChanged), this, &IQ_viewer::reprocessData);
+    connect(ui->windowFunctionComboBox, QOverload<int>::of(&QComboBox::currentIndexChanged), this, &IQ_viewer::reprocessData);
+
+    // --- range change ---
+    connect(ui->fftPlot->xAxis, SIGNAL(rangeChanged(QCPRange)), this, SLOT(clampAxisRanges()));
+    connect(ui->fftPlot->yAxis, SIGNAL(rangeChanged(QCPRange)), this, SLOT(clampAxisRanges()));
+
+
+    // --- default settings ---
+    ui->fftSizeComboBox->addItems({"256", "512", "1024", "2048", "4096", "8192"});
+    ui->windowFunctionComboBox->addItems({"Rectangular", "Hann", "Hamming", "Blackman"});
+    ui->fftSizeComboBox->setCurrentIndex(2);
+    ui->windowFunctionComboBox->setCurrentIndex(1);
+
+
+    // --- Slider Init ---
+    ui->minLevelSlider->setRange(-120, 0);
+    ui->maxLevelSlider->setRange(-120, 0);
+    ui->minLevelSlider->setValue(-100);
+    ui->maxLevelSlider->setValue(-20);
+}
+
+void IQ_viewer::generateWindow(int type, int size)
+{
+
+    // --- FFT windowing ---
+    m_window.resize(size);
+    for (int i = 0; i < size; i++)
+    {
+        switch (type)
+        {
+        case 1: // Hann
+            m_window[i] = 0.5 * (1 - cos(2 * M_PI * i / (size - 1)));
+            break;
+        case 2: // Hamming
+            m_window[i] = 0.54 - 0.46 * cos(2 * M_PI * i / (size - 1));
+            break;
+        case 3: // Blackman
+            m_window[i] = 0.42 - 0.5 * cos(2 * M_PI * i / (size - 1)) + 0.08 * cos(4 * M_PI * i / (size - 1));
+            break;
+        default: // Case 0: Rectangular
+            m_window[i] = 1.0;
+            break;
+        }
+    }
+}
+
 void IQ_viewer::on_actionOpen_triggered()
 {
-    QString fileName = QFileDialog::getOpenFileName(this, tr("Open IQ File"), "", tr("Complex Float32 (*.cf32 *.cfile *.raw)"));
+    // --- open file ---
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open IQ File"), "", tr("Complex Float32 (*.cf32 *.cfile)"));
     if (fileName.isEmpty()) {
         return;
     }
@@ -68,32 +112,45 @@ void IQ_viewer::on_actionOpen_triggered()
         return;
     }
 
+    // --- basic info ---
     ui->statusbar->showMessage(tr("Loading %1...").arg(fileName));
     QCoreApplication::processEvents();
-
 
     QByteArray data = file.readAll();
     file.close();
 
-    std::vector<std::complex<float>> iq_data;
+    m_iq_data.clear();
     const int sample_count = data.size() / sizeof(std::complex<float>);
     const auto* samples = reinterpret_cast<const std::complex<float>*>(data.constData());
+    m_iq_data.resize(sample_count);
+    std::copy(samples, samples + sample_count, m_iq_data.begin());
 
-    iq_data.resize(sample_count);
-    std::copy(samples, samples + sample_count, iq_data.begin());
-
-    ui->statusbar->showMessage(tr("Processing FFT..."));
-    QCoreApplication::processEvents();
-
-    processIQData(iq_data, m_fft_size);
+    reprocessData();
 }
 
-void IQ_viewer::processIQData(const std::vector<std::complex<float>>& iq_data, int fft_size)
+// --- when settings change ---
+void IQ_viewer::reprocessData()
+{
+    if (m_iq_data.empty()) return;
+
+    m_fft_size = ui->fftSizeComboBox->currentText().toInt();
+    int window_type = ui->windowFunctionComboBox->currentIndex();
+
+    ui->statusbar->showMessage(tr("Processing FFT with size %1...").arg(m_fft_size));
+    QCoreApplication::processEvents();
+
+    processIQData(m_iq_data, m_fft_size, window_type);
+}
+
+// --- IQ processing ---
+void IQ_viewer::processIQData(const std::vector<std::complex<float>>& iq_data, int fft_size, int window_type)
 {
     if (iq_data.empty()) {
         ui->statusbar->showMessage("No data to process.");
         return;
     }
+
+    generateWindow(window_type, fft_size);
 
     const int num_ffts = iq_data.size() / fft_size;
 
@@ -106,69 +163,103 @@ void IQ_viewer::processIQData(const std::vector<std::complex<float>>& iq_data, i
 
     for (int i = 0; i < num_ffts; ++i) {
         for (int j = 0; j < fft_size; ++j) {
-            in[j].r = iq_data[i * fft_size + j].real();
-            in[j].i = iq_data[i * fft_size + j].imag();
+            in[j].r = iq_data[i * fft_size + j].real() * m_window[j];
+            in[j].i = iq_data[i * fft_size + j].imag() * m_window[j];
         }
-
         kiss_fft(cfg, in.data(), out.data());
-
         for (int j = 0; j < fft_size; ++j) {
             int k = (j + fft_size / 2) % fft_size;
             float power = 10 * log10(out[k].r * out[k].r + out[k].i * out[k].i + 1e-12);
             m_colorMap->data()->setCell(i, j, power);
         }
     }
-
     free(cfg);
 
-    // --- Update Timeline Slider Range ---
+    // --- data range ---
+    m_xDataRange = QCPRange(0, num_ffts);
+    m_yDataRange = QCPRange(0, fft_size);
+
+    // --- full view Init ---
+    ui->fftPlot->xAxis->setRange(m_xDataRange);
+    ui->fftPlot->yAxis->setRange(m_yDataRange);
+
     ui->timelineSlider->setRange(0, num_ffts);
-    ui->timelineSlider->setValue(0);
+    ui->timelineSlider->setValue(num_ffts);
 
+    on_colorMapComboBox_currentIndexChanged(ui->colorMapComboBox->currentIndex());
+    updateColorMapRange();
 
-    // slider values for the initial color range
-    on_minLevelSlider_valueChanged(ui->minLevelSlider->value());
-    on_maxLevelSlider_valueChanged(ui->maxLevelSlider->value());
-
-
-    ui->fftPlot->rescaleAxes();
     ui->fftPlot->replot();
-
     ui->statusbar->showMessage(tr("FFT calculation complete. %1 FFTs processed.").arg(num_ffts), 5000);
 }
 
-// --- Implementation slider Slots ---
+// --- clamp plot/graph ---
+void IQ_viewer::clampAxisRanges()
+{
+    // Clamp X-Axis
+    QCPRange currentXRange = ui->fftPlot->xAxis->range();
+    if (currentXRange.lower < m_xDataRange.lower)
+    {
+        currentXRange.lower = m_xDataRange.lower;
+        currentXRange.upper = m_xDataRange.lower + currentXRange.size();
+    }
+    if (currentXRange.upper > m_xDataRange.upper)
+    {
+        currentXRange.upper = m_xDataRange.upper;
+        currentXRange.lower = m_xDataRange.upper - currentXRange.size();
+    }
+    ui->fftPlot->xAxis->setRange(currentXRange);
 
+    // Clamp Y-Axis
+    QCPRange currentYRange = ui->fftPlot->yAxis->range();
+    if (currentYRange.lower < m_yDataRange.lower)
+    {
+        currentYRange.lower = m_yDataRange.lower;
+        currentYRange.upper = m_yDataRange.lower + currentYRange.size();
+    }
+    if (currentYRange.upper > m_yDataRange.upper)
+    {
+        currentYRange.upper = m_yDataRange.upper;
+        currentYRange.lower = m_yDataRange.upper - currentYRange.size();
+    }
+    ui->fftPlot->yAxis->setRange(currentYRange);
+}
+
+// --- "zoom" ---
 void IQ_viewer::on_timelineSlider_valueChanged(int value)
 {
-    if (!m_colorMap) return;
+    if (!m_colorMap || value == 0) return;
 
-    // "Zoom"
-    double visibleWidth = m_colorMap->data()->keySize() - value;
-    ui->fftPlot->xAxis->setRange(0, visibleWidth);
+    ui->fftPlot->xAxis->setRange(0, value);
     ui->fftPlot->replot();
 }
 
-void IQ_viewer::on_minLevelSlider_valueChanged(int value)
+
+// --- colors ---
+void IQ_viewer::updateColorMapRange()
 {
     if (!m_colorMap) return;
-
-    // min is not greater than max
-    if (value >= ui->maxLevelSlider->value()) {
-        ui->maxLevelSlider->setValue(value + 1);
+    int minVal = ui->minLevelSlider->value();
+    int maxVal = ui->maxLevelSlider->value();
+    if (minVal >= maxVal) {
+        minVal = maxVal - 1;
+        ui->minLevelSlider->setValue(minVal);
     }
-    m_colorMap->setDataRange(QCPRange(value, ui->maxLevelSlider->value()));
+    m_colorMap->setDataRange(QCPRange(minVal, maxVal));
     ui->fftPlot->replot();
 }
 
-void IQ_viewer::on_maxLevelSlider_valueChanged(int value)
+// --- colors choices ---
+void IQ_viewer::on_colorMapComboBox_currentIndexChanged(int index)
 {
     if (!m_colorMap) return;
-
-    // max is not less than min
-    if (value <= ui->minLevelSlider->value()) {
-        ui->minLevelSlider->setValue(value - 1);
+    QCPColorGradient gradient;
+    switch (index)
+    {
+    case 0: gradient = QCPColorGradient::gpGrayscale; break;
+    case 1: gradient = QCPColorGradient::gpSpectrum; break;
+    case 2: gradient = QCPColorGradient::gpJet; break;
     }
-    m_colorMap->setDataRange(QCPRange(ui->minLevelSlider->value(), value));
+    m_colorMap->setGradient(gradient);
     ui->fftPlot->replot();
 }
--- IQ_viewer/iq_viewer.h
+++ IQ_viewer/iq_viewer.h
@@ -5,7 +5,7 @@
 #include <vector>
 #include <complex>
 #include "qcustomplot.h"
-#include "kiss_fft.h"
+//#include "kiss_fft.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -23,17 +23,29 @@ public:
 
 private slots:
     void on_actionOpen_triggered();
-    void processIQData(const std::vector<std::complex<float>>& iq_data, int fft_size);
+    void processIQData(const std::vector<std::complex<float>>& iq_data, int fft_size, int window_type);
+    void reprocessData();
 
-    // Slots for sliders
     void on_timelineSlider_valueChanged(int value);
-    void on_minLevelSlider_valueChanged(int value);
-    void on_maxLevelSlider_valueChanged(int value);
+    void updateColorMapRange();
+    void on_colorMapComboBox_currentIndexChanged(int index);
 
+    // clamp slot
+    void clampAxisRanges();
 
 private:
+    void setupUiControls();
+    void generateWindow(int type, int size);
+
     Ui::IQ_viewer *ui;
     QCPColorMap *m_colorMap;
+
+    std::vector<std::complex<float>> m_iq_data;
+    std::vector<float> m_window;
     int m_fft_size;
+
+    // clamped valid raange
+    QCPRange m_xDataRange;
+    QCPRange m_yDataRange;
 };
 #endif // IQ_VIEWER_H
